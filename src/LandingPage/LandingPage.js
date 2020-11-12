16b09291eeee309496514a4fe48a2bef8518fb02
dspirovski <dspirovski@hotmail.com>
2020-11-12 21:14:12 +0100

    Landingpage file

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './LandingPage.scss';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Navbar from './Navbar'
+import Sign_in from './Sign'
+
+
+function App() {
+  return (
+    <div className="front-page">
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/Sign_in" component={Sign_in} />
+        </Switch>
+      </BrowserRouter>
+
+
+    </div>
+
+
+  );
+}
+
+export default App;
