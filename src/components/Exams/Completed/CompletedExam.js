4664c9c7c6fbbb05c221bb232aceba7fffb994e0
dspirovski <dspirovski@hotmail.com>
2020-11-30 23:01:21 +0100

    CompleatedExam js and css files

--- src/components/Exams/Completed/CompletedExam.js
+++ src/components/Exams/Completed/CompletedExam.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import styles from './CompletedExam.module.css';
 
 function CompletedExam() {
 	return (
 		<div>
-			Completed exam here
+			<h1>Micro-organisms</h1>
+			<h6>Higher secondary</h6>
+			<div className={styles.dots3}>
+				<div className={styles.circle3}></div>
+				<div className={styles.line3}></div>
+				<div className={styles.circle3}></div>
+				<div className={styles.line3}></div>
+				<div className={styles.circle3}></div>
+				<div className={styles.line3}></div>
+				<div className={styles.circle3}></div>
+			</div>
 		</div>
 	)
 }
