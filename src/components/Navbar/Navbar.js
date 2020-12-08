19c451b0682528b723986424a9df2003b84ecbe6
dspirovski <dspirovski@hotmail.com>
2020-12-08 20:26:17 +0100

    navbar code update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -37,48 +37,9 @@ class Navbar extends React.Component {
 					<li>Start Learning</li>
 				</ul>
 			</div>
+
 		)
 	}
 }
 
 export default Navbar;
-
-/*
-function Navbar() {
-		return (
-				<div className={classes.navbar} style={{ backgroundImage: 'url("background-image.png")' }}>
-						<ul className={classes.navbarList}>
-								<li><Link to="/" onClick={App}>Fuelup education</Link></li>
-								<li>Courses&#9662;
-								<ul className={classes.dropdownMenu}>
-												<li className={classes.dropdownChild}>Junior High School</li>
-												<li className={classes.dropdownChild}>Senior Hign School</li>
-												<li className={classes.dropdownChild}>Vacational School</li>
-												<li className={classes.dropdownChild}>College Students</li>
-												<li className={classes.dropdownChild}>Musical School</li>
-												<li className={classes.dropdownChild}>Engineering School</li>
-										</ul>
-								</li>
-								<li>Subjects&#9662;
-								<ul className={classes.dropdownMenu}>
-												<li className={classes.dropdownChild}>Maths</li>
-												<li className={classes.dropdownChild}>History</li>
-												<li className={classes.dropdownChild}>English</li>
-												<li className={classes.dropdownChild}>Hindi</li>
-												<li className={classes.dropdownChild}>Regional language</li>
-												<li className={classes.dropdownChild}>Commerces</li>
-												<li className={classes.dropdownChild}>Science</li>
-												<li className={classes.dropdownChild}>Environment science</li>
-										</ul>
-								</li>
-								<li>For Kids</li>
-								<li>Team</li>
-								<li><Link to="/Sign" onClick={Sign}>Sign in</Link></li>
-								<li>Start Learning</li>
-						</ul>
-				</div >
-		)
-}
-
-export default Navbar
-*/
