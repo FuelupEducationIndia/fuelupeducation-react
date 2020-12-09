267a1f7f8f008cbb68321704b726ee0c947474e0
dspirovski <dspirovski@hotmail.com>
2020-12-09 23:52:57 +0100

    code update

--- src/components/Exams/Completed/CompletedExam.js
+++ src/components/Exams/Completed/CompletedExam.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './CompletedExam.module.css';
+import Navbar from '../../Navbar/Navbar'
 
 function CompletedExam() {
 	return (
-		<div>
+		<div className={styles.completedExam}>
+			<Navbar />
 			<h1>Micro-organisms</h1>
 			<h6>Higher secondary</h6>
 			<div className={styles.dots3}>
@@ -14,6 +16,11 @@ function CompletedExam() {
 				<div className={styles.circle3}></div>
 				<div className={styles.line3}></div>
 				<div className={styles.circle3}></div>
+			</div>
+			<div className={styles.footer}>
+				<button>VIEW</button>
+				<span>Dashboard</span>
+				<button>&#x3e;</button>
 			</div>
 		</div>
 	)
