795e78e527d0a5fc078a287177b9b1223cf59fe8
dspirovski <dspirovski@hotmail.com>
2020-12-09 23:46:49 +0100

    code update

--- src/components/Exams/CreateSections/CreateSections.js
+++ src/components/Exams/CreateSections/CreateSections.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './CreateSections.module.css';
 import { Link } from 'react-router-dom';
+import Navbar from "../../Navbar/Navbar"
 
 const CreateSections = () => {
 	return (
 		<div className={styles.createSections}>
+			<Navbar />
 			<h1>Micro-organisms</h1>
 			<h6>Higher secondary</h6>
 			<div className={styles.dots4}>
