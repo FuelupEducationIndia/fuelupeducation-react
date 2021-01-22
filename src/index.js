0eaa386a1ecd8159442ba3e688533858c9844f7b
priyanka <priyankaguptaphp@mycontactsca.com>
2021-01-22 19:37:07 +0530

    test branch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './sass/main.scss'
+//import './sass/main.scss'
 
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker'
-import { BrowserRouter } from 'react-router-dom'
+import {
+    BrowserRouter
+} from 'react-router-dom'
 
-const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+const app = ( <
+    BrowserRouter >
+    <
+    App / >
+    <
+    /BrowserRouter>
 )
 
 ReactDOM.render(app, document.getElementById('root'))
