c494a1a6ebaeecc905f14449cf6822ae61a59bbb
vaibhav_jogdand <vrjogdand708@gmail.com>
2021-09-01 13:49:36 +0530

    Initial Commit

---
+++ src/components/header/header.js
@@ -0,0 +1,9 @@
+import './header.scss'
+
+export default function Header(){
+    return(
+        <div>
+            Hello world!!
+        </div>
+    );
+}
---
+++ src/components/topbar/topbar.js
@@ -0,0 +1,28 @@
+import './topbar.scss'
+
+export default function Topbar(){
+    return(
+        <div className='topbar'>
+            <div className='logo'>
+
+            </div>
+            <div className='topbarItems'>
+
+                <ul className='topbarList'>
+
+                    <li topbarItem><a href=""></a></li>
+                    <li topbarItem><a href=""></a></li>
+                    <li topbarItem><a href=""></a></li>
+                    <li topbarItem><a href=""></a></li>
+                    <li topbarItem><a href=""></a></li>
+                    <li topbarItem><a href=""></a></li>
+
+                </ul>
+
+            </div>
+            <div className='topbarProfileItems'>
+
+            </div>
+        </div>
+    );
+}
