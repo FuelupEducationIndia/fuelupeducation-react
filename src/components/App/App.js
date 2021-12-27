17b3b3b0c08b51c61bc8278bc4f097a85f33ba88
ermilla <wteffera11@gmail.com>
2021-12-27 10:40:48 +0300

    A link to the courses Page is added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Route, Switch, useLocation } from 'react-router-dom'
 
@@ -14,14 +14,15 @@ import LoadingSimple from '../UI/LoadingSimple/LoadingSimple'
 
 // Routes and Layout for public/unauthorized user
 import Layout from '../Layouts/PublicLayout'
+// import student from '../../FuelUpEducation student-side/src/App'
 
 const App = ({ title }) => {
   // Get current location. if it's homepage =>  rendering landing
   const location = useLocation()
-
+  const [role, setRole] = useState(true)
   let layoutToDisplay = ''
 
-  layoutToDisplay = <Layout />
+  layoutToDisplay = role === true ? <Layout /> : <h1> I am ermias</h1>
 
   return (
     <Provider store={store}>
